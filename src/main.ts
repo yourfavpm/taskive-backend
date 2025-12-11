7acfe43abbd432f472d1fe920fc8d7d69796c559
Benita Eze <theexceptionalpm@gmail.com>
2025-12-11 10:27:43 +0000

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ⚡ Enable CORS
+  app.enableCors({
+    origin: '*' // For testing, allow all origins. Later, replace '*' with your frontend URL
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
